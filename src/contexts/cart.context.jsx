e3a5219b2fb7206714f87851c2a55d7511baf0e8
Cristi Marcu <cristi.marcu41@gmail.com>
2025-10-17 14:17:57 +0300

    added the addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,32 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
+const addCartItem=(cartItems, productToAdd)=>{
+    // find if cart items contain Product to add
+    const existingCartItem=cartItems.find(item=>item.id===productToAdd.id);
+    // if found, increment quantity
+    if (existingCartItem) {
+        // Return new array with incremented quantity for matched product
+        return cartItems.map(item =>item.id === productToAdd.id ? { ...item, quantity: item.quantity + 1 } : item);
+    }
+    return [...cartItems, {...productToAdd, quantity:1}]
+}
 export const CartContext=createContext({
     isCartOpen:false,
-    setIsCartOpen: ()=>{}
+    setIsCartOpen: ()=>{},
+    cartItems:[],
+    addItemToCart:()=>{},
+    cartCount:0,
 });
 
 export const CartProvider=({children})=>{
     const [isCartOpen, setIsCartOpen]=useState(false);
-
-    return <CartContext.Provider value={{isCartOpen, setIsCartOpen}}>{children}</CartContext.Provider>
+    const [cartItems,setCartItems]=useState([]);
+    const [cartCount,setCartCount]=useState(0);
+    useEffect(()=>{
+        const newCartCount=cartItems.reduce((total, cartItem)=>total+cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    }, [cartItems])
+    const addItemToCart=(productToAdd)=>{
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+    return <CartContext.Provider value={{isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount}}>{children}</CartContext.Provider>
 }
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,19 @@
 import {useContext} from 'react';
 
 import { ProductsContext } from '../../contexts/products.context';
+// import { CartContext } from '../../contexts/cart.context';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './shop.styles.scss'
 
 const Shop = () => {
     const {products}=useContext(ProductsContext);
+    // const {addItemToCart}=useContext(CartContext);
     return (
-        <div>
-            {products.map(({id,name})=>(
-                <div key="{id}"><h2>{name}</h2></div>
+        <div className='products-container'>
+            {products.map((product)=>(
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     );
